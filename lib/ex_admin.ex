3fc738293e1ccd9d13d2c946ec9ba0541c108715
Stephen M. Pallen <smpallen99@yahoo.com>
2017-05-12 11:21:10 -0400

    initial commit

---
+++ lib/ex_admin.ex
@@ -0,0 +1,18 @@
+defmodule ExAdmin do
+  @moduledoc """
+  Documentation for ExAdmin.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAdmin.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_admin_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAdminTest do
+  use ExUnit.Case
+  doctest ExAdmin
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
