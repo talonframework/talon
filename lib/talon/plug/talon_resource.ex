f81b645d4d65d8f6d678eeadd0b676f346a3d8d3
Daniel Snider <dsnider@digitaltoniq.com>
2017-06-12 20:02:10 -0400

    Ensure either talon_resource or talon_page is present in TalonResource plug

--- lib/talon/plug/talon_resource.ex
+++ lib/talon/plug/talon_resource.ex
@@ -15,7 +15,9 @@ defmodule Talon.Plug.TalonResource do  # TODO: rename to something more generic 
     context = opts[:talon] || talon[:talon] || raise("talon option required")
 
     talon_resource = context.talon_resource(conn.params["resource"])
-    talon_page = if talon_resource, do: nil, else: context.talon_page(conn.params["page"])
+    talon_page = if talon_resource in [nil, []], do: context.talon_page(conn.params["page"])
+
+    talon_resource || talon_page || raise("resource or page required")
 
     assign conn, :talon, Enum.into([talon_resource: talon_resource, talon_page: talon_page], talon)
   end
