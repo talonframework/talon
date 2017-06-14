11ac91be6459c5bb3a4090213880fc1660ef8d15
Daniel Snider <dsnider@digitaltoniq.com>
2017-06-13 20:41:04 -0400

    Remove cruft from Page #52

--- lib/talon/page.ex
+++ lib/talon/page.ex
@@ -11,30 +11,12 @@ defmodule Talon.Page do
     context = opts[:context]
 
     quote do
-      # @__module__ unquote(schema) # TODO: remove (DJS)
-
       @__context__ unquote(context) || (Module.split(__MODULE__) |> hd |> Module.concat(nil))
-
-      # @__params_key__  Module.split(@__module__) |> List.last |> to_string |> Inflex.underscore  TODO: needed? (DJS)
-      # @__route_name__ @__params_key__ |> Inflex.Pluralize.pluralize
 
       @spec index_card_title() :: String.t
       def index_card_title do
-        # TODO: find a resuable approach here
-
-        # Inflex.Pluralize.pluralize "#{Module.split(@__module__) |> List.last}"
-
-        "Dashboard" # TODO: (DJS)
+        Inflex.Pluralize.pluralize "#{Module.split(__MODULE__) |> List.last}"
       end
-
-    #   @spec route_name() :: String.t
-    #   def route_name, do: @__route_name__
-
-    #   @spec params_key() :: String.t
-    #   def params_key, do: @__params_key__
-
-      # @spec schema() :: Module.t    # TODO: remove (DJS)
-      # def schema, do: nil # @__module__
 
       @doc """
       Returns a list of links for each of the Talon dashboards.
@@ -45,7 +27,7 @@ defmodule Talon.Page do
       def dashboard_paths(%{talon: talon} = _talon) do
         talon.dashboard_names()
         |> Enum.map(fn dashboard ->
-          {Talon.Utils.titleize(dashboard),  "/talon/#{dashboard}"} # TODO: Talon.Utils.talon_dashboard_path (DJS)
+          {Talon.Utils.titleize(dashboard),  "/talon/#{dashboard}"}
         end)
       end
 
@@ -58,24 +40,20 @@ defmodule Talon.Page do
       def resource_paths(%{talon: talon} = _talon) do
         talon.resources()
         |> Enum.map(fn talon_resource ->
-          schema = talon_resource.schema() # TODO: let view determine presentation name for a resource
+          schema = talon_resource.schema() # TODO: let view determine presentation name for a resource (DJS)
           {Talon.Utils.titleize(schema) |> Inflex.Pluralize.pluralize, Talon.Utils.talon_resource_path(schema, :index)}
         end)
       end
 
       @doc """
-      Returrn the Talon context.
+      Return the Talon context.
       """
       @spec context() :: atom
       def context, do: @__context__
 
-      # TODO: (DJS)
-    #   defoverridable [
-    #     resource_paths: 1, nav_action_links: 2, params_key: 0, display_schema_columns: 1,
-    #     index_card_title: 0, form_card_title: 1, tool_bar: 0, route_name: 0, repo: 0,
-    #     adapter: 0, render_column_name: 2, get_schema_field: 3, preload: 3, context: 0,
-    #     paginate: 3, query: 3, search: 1, search: 3, schema_types: 0, name_field: 0
-    #   ]
+      defoverridable [
+        context: 0, resource_paths: 1, dashboard_paths: 1, index_card_title: 0
+      ]
     end
   end
 end
