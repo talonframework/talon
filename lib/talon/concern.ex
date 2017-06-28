162deae6b1d0ee7167e6e929e0a9ee2cfe24592b
Daniel Snider <dsnider@digitaltoniq.com>
2017-06-27 23:54:11 -0400

    Add ability to override a concern’s default page

--- lib/talon/concern.ex
+++ lib/talon/concern.ex
@@ -296,7 +296,7 @@ defmodule Talon.Concern do
         base: 0, repo: 0, resource_map: 0, schema: 1, schema_names: 0,
         talon_resource: 1, resource_path: 3, resource_schema: 1,
         controller_action: 1, template_path_name: 1, schema_field_type: 3,
-        nav_action_links: 1, messages_backend: 0
+        nav_action_links: 1, messages_backend: 0, default_page: 0
       ]
     end
 
