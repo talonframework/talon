10f894bd118a590f23a2f305f9847aecc597634b
Daniel Snider <dsnider@digitaltoniq.com>
2018-04-16 14:35:51 -0400

    Allow Talon to optionally be referenced as an Umbrella app #95

--- mix.exs
+++ mix.exs
@@ -1,16 +1,40 @@
 defmodule Talon.Mixfile do
   use Mix.Project
 
+  # def project do
+  #   [app: :talon,
+  #    version: "0.1.0",
+  #    elixir: "~> 1.4",
+  #    elixirc_paths: elixirc_paths(Mix.env),
+  #    build_embedded: Mix.env == :prod,
+  #    start_permanent: Mix.env == :prod,
+  #    compilers: compilers(Mix.env),
+  #    dialyzer: [plt_add_deps: :transitive],
+  #    deps: deps()]
+  # end
+
+  # Testing out Umbrella approach
   def project do
-    [app: :talon,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     compilers: compilers(Mix.env),
-     dialyzer: [plt_add_deps: :transitive],
-     deps: deps()]
+    base_config = [
+      app: :talon,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      compilers: compilers(Mix.env),
+      dialyzer: [plt_add_deps: :transitive],
+      deps: deps()
+    ]
+
+    umbrella_config = [
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+    ]
+
+    if in_umbrella?(File.cwd!), do: base_config ++ umbrella_config, else: base_config
   end
 
   defp compilers(:test), do: [:phoenix] ++ Mix.compilers
@@ -40,5 +64,14 @@ defmodule Talon.Mixfile do
       {:ecto_talon, github: "talonframework/ecto_talon"},
       # {:ecto_talon, path: "../ecto_talon", only: :test},
     ]
+  end
+
+  # From Mix.Phoenix
+  def in_umbrella?(app_path) do
+    umbrella = Path.expand(Path.join [app_path, "..", ".."])
+    mix_path = Path.join(umbrella, "mix.exs")
+    apps_path = Path.join(umbrella, "apps")
+    IO.puts apps_path
+    File.exists?(mix_path) && File.exists?(apps_path)
   end
 end
