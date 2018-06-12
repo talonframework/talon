f9607deb45154491d5d215990f45dc67265fcbaa
Daniel Snider <dsnider@digitaltoniq.com>
2018-06-12 19:56:06 -0400

    Don’t blow existing params when paginating

--- priv/templates/talon.gen.components/components/paginate/views/paginate_view.ex
+++ priv/templates/talon.gen.components/components/paginate/views/paginate_view.ex
@@ -5,13 +5,13 @@ defmodule <%= base %>.<%= concern %>.<%= theme_module %>.<%= web_namespace %>Pag
   import Talon.Utils, only: [to_integer: 1]
 
   def paginate(%{params: params} = conn) do
+    link = Talon.Concern.resource_path conn, :index, [Map.drop(params, ["resource", "page"])]
     page_number = to_integer(params["page"] || 1)
     page = conn.assigns[:page]
     model_name =
       conn.assigns[:resource]
       |> Module.split
       |> List.last
-    link = Talon.Concern.resource_path conn, :index, [[order: nil]]
 
     paginate(link, page_number, page.page_size, page.total_pages, page.total_entries, model_name, show_information: true)
   end
