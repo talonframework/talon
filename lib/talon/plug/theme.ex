ef46c974496908b003ede72b0f18400fef571978
Daniel Snider <dsnider@digitaltoniq.com>
2018-09-19 10:59:18 -0400

    Theme switching hack

--- lib/talon/plug/theme.ex
+++ lib/talon/plug/theme.ex
@@ -1,5 +1,7 @@
 defmodule Talon.Plug.Theme do
   import Plug.Conn
+  require Talon.Config, as: Config
+  import Phoenix.Controller, only: [put_flash: 3, get_flash: 1]
 
   @behaviour Plug
 
@@ -9,6 +11,25 @@ defmodule Talon.Plug.Theme do
 
   def call(conn, opts) do
     talon = conn.assigns[:talon]
+    new_theme = conn.params["theme"]
+
+    # TODO: hack, add theme switching to controller
+    conn =
+      if new_theme do
+        if (new_theme in Config.themes(:talon)) do
+          Application.put_env(:admin, Admin.Admin, theme: new_theme)
+          conn
+        else
+          Config.themes(:talon)
+          # |> IO.inspect(label: "error setting theme")
+
+          # TODO: Talon.Concern.concern(conn).messages_backend().theme_does_not_exist(theme)
+          put_flash(conn, :error, "The theme \"#{new_theme}\" does not exist")
+        end
+        # assign conn, :params, Map.delete(conn.params, "theme")
+      else
+        conn
+      end
     assign conn, :talon, Map.put(talon, :theme, opts[:theme] || talon.concern.theme())
   end
 end
