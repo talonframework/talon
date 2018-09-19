74563c7565a38b195ca590d302bc12194af3cf46
Daniel Snider <dsnider@digitaltoniq.com>
2018-09-19 10:50:39 -0400

    Add default view support

--- lib/talon/plug/view.ex
+++ lib/talon/plug/view.ex
@@ -14,7 +14,20 @@ defmodule Talon.Plug.View do
           theme = Inflex.camelize talon.theme
           prefix = talon[:talon_resource] |> Module.split |> List.last
 
-          Module.concat [talon.concern, theme, conn.assigns.talon.web_namespace, prefix <> "View"]
+          # FIXME: quick and dirty default view handling
+
+          {:ok, modules} = :application.get_key(:admin, :modules)
+
+          canonical_view = Module.concat [talon.concern, theme, conn.assigns.talon.web_namespace, prefix <> "View"]
+
+          default_view = Module.concat [talon.concern, theme, conn.assigns.talon.web_namespace, "DefaultView"]
+
+          all_mods_with_default_last = (modules -- [default_view]) ++ [default_view]
+
+          all_mods_with_default_last |> Enum.find(fn(element) -> element in [canonical_view, default_view] end)
+
+          # prefix = "Default"
+          # Module.concat [talon.concern, theme, conn.assigns.talon.web_namespace, prefix <> "View"]
         view ->
           view
       end
